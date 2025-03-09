83558e1553ca0630faecaaccb104c6aff9f9ef49
xiewenbin <121350551@qq.com>
2025-03-09 18:20:13 +0800

    init 补全

---
+++ openai-code-review-sdk/src/main/java/org/example/sdk/OpenAiCodeReview.java
@@ -0,0 +1,14 @@
+package org.example.sdk;
+
+/**
+ * @Classname OpenAiCodeReview
+ * @Description TODO
+ * @Date 2025/3/9 17:16
+ * @Created by 12135
+ */
+public class OpenAiCodeReview {
+
+    public static void main(String[] args) {
+        System.out.println("测试执行");
+    }
+}
---
+++ openai-code-review-sdk/src/test/java/org/example/sdk/ApiTest.java
@@ -0,0 +1,7 @@
+package org.example.sdk;
+public class ApiTest {
+
+    public static void main(String[] args) {
+
+    }
+}
